28d5f1570c3fa1e86cd246feb38d79f1c256e7bb
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2024-04-01 13:38:32 +0700

    Initial Commit

---
+++ railrest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  railrest
+//
+//  Created by Reyhan Ariq Syahalam on 01/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ railrest/railrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  railrestApp.swift
+//  railrest
+//
+//  Created by Reyhan Ariq Syahalam on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct railrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
