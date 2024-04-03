31707e504d0c1f150bf975f151cf1782e2580f59
Evelyn Santoso <velyn.es24@gmail.com>
2024-04-03 17:23:36 +0700

    summary page

---
+++ railrest/Views/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  railrest
+//
+//  Created by Evelyn Santoso on 03/04/24.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
