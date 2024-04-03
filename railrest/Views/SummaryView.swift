43a8ad28e47bc380f41a22acf51ce7c3b45227ae
Evelyn Santoso <velyn.es24@gmail.com>
2024-04-03 17:24:07 +0700

    summary view

--- railrest/Views/SummaryView.swift
+++ railrest/Views/SummaryView.swift
@@ -9,9 +9,56 @@ import SwiftUI
 
 struct SummaryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack {
+                VStack(alignment: .center, spacing: 13) {
+                    Spacer()
+                    VStack(alignment: .center, spacing: 16.0) {
+                        Text("You've Slept For")
+                            .font(Font.custom("SF Pro", size: 34))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .frame(width: 297, alignment: .top)
+                        
+                        //ganti variabel
+                        Text("10s")
+                            .font(.system(size: 40))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.white)
+                            .frame(width: 297, alignment: .top)
+                    }.padding()
+                    
+                    Spacer()
+                    NavigationLink(destination: InitialView()) {
+                        
+                        Text("Back to Main Screen")
+                            .font(Font.custom("SF Pro", size: 17))
+                            .foregroundColor(.black)
+                    }
+                    
+                  
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 14)
+                    .frame(width: 361, alignment: .center)
+                    .background(Color(hex:"FFCC00"))
+                    .cornerRadius(12)
+                    
+                    // Bottom Space
+                    Spacer()
+                        .frame(minHeight: 16, idealHeight: 40, maxHeight: .infinity)
+                        .fixedSize()
+                }
+                .padding()
+            }
+            .frame(width: 393, height: 852)
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color(hex : "036B46"), Color(hex : "2DAA7D")]), startPoint: .top, endPoint: .bottom)
+                )
+            }
+        }
     }
-}
+
 
 #Preview {
     SummaryView()
