6e0624420c32d38a75a79c60d8dc7036e7501737
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2024-04-03 14:25:37 +0700

    ganti model jam

--- railrest/Views/OngoingView.swift
+++ railrest/Views/OngoingView.swift
@@ -24,48 +24,50 @@ struct OngoingView: View {
                 
                 ZStack {
                     VStack {
-                        VStack {
-                            Image("nap")
-                                .resizable()
-                                .frame(width: 220, height: 215)
-                            
-                            Text("Power Napping for...")
-                                .font(.title)
-                                .foregroundColor(.white)
-                            
+                        VStack (alignment: .center, spacing: 10) {
                             TimerView(timerManager: timerManager)
                                 .foregroundColor(.white)
                             
-                            Button(action: {
-                                presentationMode.wrappedValue.dismiss()
-                            }) {
-                                Text("Cancel Nap")
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                    .padding(15)
-                                    .background(RoundedRectangle(cornerRadius: 40).fill(.yellow))
-                                    .foregroundColor(Color(hex: "101A4D"))
-                            }
+                            Text("Remaining Nap Time")
+                                .font(
+                                    Font.custom("SF Pro", size: 20)
+                                        .weight(.semibold)
+                                )
+                                .foregroundColor(.white)
+                            
+                            Text("Your Optimal Nap Time is 12 Minutes")
+                              .font(Font.custom("SF Pro", size: 13))
+                              .foregroundColor(.white.opacity(0.65))
+                            
+                            
+//                            Button(action: {
+//                                presentationMode.wrappedValue.dismiss()
+//                            }) {
+//                                Text("Cancel Nap")
+//                                    .font(.title3)
+//                                    .fontWeight(.semibold)
+//                                    .padding(15)
+//                                    .background(RoundedRectangle(cornerRadius: 40).fill(.yellow))
+//                                    .foregroundColor(Color(hex: "101A4D"))
+//                            }
                         }
-                        .padding(.horizontal, 60)
-                        .padding(.vertical, 25)
-                        .background(
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(LinearGradient(gradient: Gradient(colors: [Color(hex: "3D47C9"), Color(hex: "1E2363")]), startPoint: .top, endPoint: .bottom))
-                                .shadow(radius: 25)
-                        )
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 0)
+                        .frame(width: 360, height: 360, alignment: .center)
+                        .background(Color(red: 0.16, green: 0.19, blue: 0.52))
+                        .cornerRadius(231)
                         
-
+                        
                         Text("Enjoy and relax to get a better sleep")
                             .font(.subheadline)
-//                            .padding()
+                        //                            .padding()
                             .foregroundColor(Color(hex: "63FFF6"))
                         
                         Text("Calm Rain")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.title)
+                            .fontWeight(.bold)
                             .foregroundColor(.white)
-//                            .padding(.top, 20)
+                        //                            .padding(.top, 20)
                         
                         Text("is playing..")
                             .foregroundColor(.white)
@@ -80,12 +82,6 @@ struct OngoingView: View {
                     }
                     .padding(15)
                     .navigationBarBackButtonHidden(true)
-                    .toolbar {
-                        ToolbarItem(placement: .principal) {
-                            Image("railrest_white")
-                                .padding(.top)
-                        }
-                    }
                 }
                 .frame(width: geometry.size.width, height: geometry.size.height)
                 .background(Color(hex: "1E2363"))
