f403783b0e6d7d6f839003cf0cfd85ca60538b4f
Reyhan Ariq Syahalam <ariqsyahalam@risya.id>
2024-04-03 17:55:52 +0700

    conflict solved

--- railrest/Views/OngoingView.swift
+++ railrest/Views/OngoingView.swift
@@ -30,9 +30,6 @@ struct OngoingView: View {
                             TimerView(timerManager: timerManager)
                                 .foregroundColor(.white)
                             
-                            //                            TimerView(timerManager: timerManager)
-                            //                                .foregroundColor(.white)
-                            
                             Text("Remaining Nap Time")
                                 .font(
                                     Font.custom("SF Pro", size: 20)
@@ -41,59 +38,45 @@ struct OngoingView: View {
                                 .foregroundColor(.white)
                             
                             Text("Your Optimal Nap Time is 12 Minutes")
-                              .font(Font.custom("SF Pro", size: 13))
-                              .foregroundColor(.white.opacity(0.65))
-                            
-                            
-//                            Button(action: {
-//                                presentationMode.wrappedValue.dismiss()
-//                            }) {
-//                                Text("Cancel Nap")
-//                                    .font(.title3)
-//                                    .fontWeight(.semibold)
-//                                    .padding(15)
-//                                    .background(RoundedRectangle(cornerRadius: 40).fill(.yellow))
-//                                    .foregroundColor(Color(hex: "101A4D"))
-//                            }
+                                .font(Font.custom("SF Pro", size: 13))
+                                .foregroundColor(.white.opacity(0.65))
                         }
-                        .padding(.horizontal, 60)
-                        .padding(.vertical, 25)
-                        .background(
-                            RoundedRectangle(cornerRadius: 20)
-                                .fill(LinearGradient(gradient: Gradient(colors: [Color(hex: "3D47C9"), Color(hex: "1E2363")]), startPoint: .top, endPoint: .bottom))
-                                .shadow(radius: 25)
-                        )
-                        
-                        
-                        Text("Choose the ambience to help you sleep")
-                            .font(.body)
-                            .padding(16)
-                            .foregroundColor(.white)
-                        
-                        
-                        VStack {
-                            HStack {
-                                OptionButton(imageName: "cloud.rain", label: "Rain", soundType: "rain", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
-                                OptionButton(imageName: "radio", label: "Static", soundType: "static", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
-                                OptionButton(imageName: "nosign", label: "None", soundType: "none", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
-                            }
-                        }
-                        .onAppear {
-                            // Pastikan activeSoundType tidak nil
-                            if activeSoundType == nil {
-                                activeSoundType = "none"
-                            }
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 0)
+                        .frame(width: 360, height: 360, alignment: .center)
+                        .background(Color(red: 0.16, green: 0.19, blue: 0.52))
+                        .cornerRadius(231)
+                    
+                    
+                    Text("Choose the ambience to help you sleep")
+                        .font(.body)
+                        .padding(16)
+                        .foregroundColor(.white)
+                    
+                    
+                    VStack {
+                        HStack {
+                            OptionButton(imageName: "cloud.rain", label: "Rain", soundType: "rain", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
+                            OptionButton(imageName: "radio", label: "Static", soundType: "static", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
+                            OptionButton(imageName: "nosign", label: "None", soundType: "none", activeSoundType: $activeSoundType, currentPlayingSound: $currentPlayingSound)
                         }
                     }
-                    .padding(15)
-                    .navigationBarBackButtonHidden(true)
+                    .onAppear {
+                        // Pastikan activeSoundType tidak nil
+                        if activeSoundType == nil {
+                            activeSoundType = "none"
+                        }
+                    }
                 }
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .background(Color(hex: "1E2363"))
+                .padding(15)
+                .navigationBarBackButtonHidden(true)
             }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .background(Color(hex: "1E2363"))
         }
-        
     }
+    
+}
 }
 
 #Preview {
